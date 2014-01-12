8caed66439b0040e6cfa2409c5272bed9ae8d3e0
Aleksi Sapon <QcTechs@gmail.com>
2014-01-12 12:28:22 -0500

    Fix license headers

--- api/src/main/java/org/spout/renderer/api/gl/Texture.java
+++ api/src/main/java/org/spout/renderer/api/gl/Texture.java
@@ -24,40 +24,14 @@
  * License and see <http://spout.in/licensev1> for the full license, including
  * the MIT license.
  */
-/*
- * This file is part of Caustic.
- *
- * Copyright (c) 2013 Spout LLC <http://www.spout.org/>
- * Caustic is licensed under the Spout License Version 1.
- *
- * Caustic is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * In addition, 180 days after any changes are published, you can use the
- * software, incorporating those changes, under the terms of the MIT license,
- * as described in the Spout License Version 1.
- *
- * Caustic is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
- * more details.
- *
- * You should have received a copy of the GNU Lesser General Public License,
- * the MIT license and the Spout License Version 1 along with this program.
- * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
- * License and see <http://spout.in/licensev1> for the full license, including
- * the MIT license.
- */
 package org.spout.renderer.api.gl;
 
 import java.nio.ByteBuffer;
 
 import org.spout.renderer.api.Creatable;
 import org.spout.renderer.api.GLVersioned;
-import org.spout.renderer.api.data.VertexAttribute.DataType;
 import org.spout.renderer.api.data.Color;
+import org.spout.renderer.api.data.VertexAttribute.DataType;
 
 /**
  * Represents a texture for OpenGL. Image data can be set with one of the <code>setImageData(...)</code> methods before creation, but this is not obligatory. This results in an empty texture, with an
