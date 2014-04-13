3967d82dc5ebdd159ad93c9adaa31e1b7cbbffe2
Aleksi Sapon <alek42008@yahoo.com>
2014-04-12 21:43:15 -0400

    Add deployNatives to LWJGL
    Deploys the native libraries to the given directory and sets the path
    in the properties

--- lwjgl/src/main/java/org/spout/renderer/lwjgl/LWJGLUtil.java
+++ lwjgl/src/main/java/org/spout/renderer/lwjgl/LWJGLUtil.java
@@ -26,6 +26,11 @@
  */
 package org.spout.renderer.lwjgl;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
 
@@ -42,8 +47,78 @@ public final class LWJGLUtil {
     public static final GLImplementation GL21_IMPL = new GLImplementation(GLVersion.GL21, GL21Context.class.getName());
     public static final GLImplementation GL30_IMPL = new GLImplementation(GLVersion.GL30, GL30Context.class.getName());
     public static final GLImplementation GL32_IMPL = new GLImplementation(GLVersion.GL32, GL32Context.class.getName());
+    private static final String[] WINDOWS_NATIVE_LIBRARIES = {
+            "jinput-dx8_64.dll", "jinput-dx8.dll", "jinput-raw_64.dll", "jinput-raw.dll",
+            "jinput-wintab.dll", "lwjgl.dll", "lwjgl64.dll", "OpenAL32.dll", "OpenAL64.dll"
+    };
+    private static final String WINDOWS_NATIVES_DIRECTORY = "natives/windows/";
+    private static final String[] MACOSX_NATIVE_LIBRARIES = {
+            "libjinput-osx.jnilib", "liblwjgl.jnilib", "openal.dylib"
+    };
+    private static final String MACOSX_NATIVES_DIRECTORY = "natives/mac/";
+    private static final String[] LINUX_NATIVE_LIBRARIES = {
+            "liblwjgl.so", "liblwjgl64.so", "libopenal.so", "libopenal64.so", "libjinput-linux.so",
+            "libjinput-linux64.so"
+    };
+    private static final String LINUX_NATIVES_DIRECTORY = "natives/linux/";
 
     private LWJGLUtil() {
+    }
+
+    /**
+     * Attempts to deploy the LWJGL native libraries into the given directory. The sub-directory "natives/PLATFORM_NAME" (where PLATFORM_NAME is the name of the platform) will be created inside, and
+     * will contain the natives. The path to the natives property will be set, but the libraries not loaded. If any error is encountered while copying the natives, the operation will be aborted and
+     * the method will return false.
+     *
+     * @param directory The directory into which to deploy the natives. Can be null if the path to use is the working directory
+     * @return Whether or not the operation succeeded.
+     */
+    public static boolean deployNatives(File directory) {
+        final String[] nativeLibs = getNativeLibraries();
+        final String nativesDirectory = getNativesDirectory();
+        final File nativesDir = new File(directory, nativesDirectory);
+        nativesDir.mkdirs();
+        for (String nativeLib : nativeLibs) {
+            final File nativeFile = new File(nativesDir, nativeLib);
+            if (!nativeFile.exists()) {
+                try {
+                    Files.copy(LWJGLUtil.class.getResourceAsStream("/" + nativeLib), nativeFile.toPath());
+                } catch (IOException ex) {
+                    CausticUtil.getCausticLogger().log(Level.SEVERE, "Failed to copy native library file", ex);
+                    return false;
+                }
+            }
+        }
+        final String nativesPath = nativesDir.getAbsolutePath();
+        System.setProperty("org.lwjgl.librarypath", nativesPath);
+        System.setProperty("net.java.games.input.librarypath", nativesPath);
+        return true;
+    }
+
+    private static String[] getNativeLibraries() {
+        final int platform = org.lwjgl.LWJGLUtil.getPlatform();
+        if (platform == org.lwjgl.LWJGLUtil.PLATFORM_WINDOWS) {
+            return WINDOWS_NATIVE_LIBRARIES;
+        } else if (platform == org.lwjgl.LWJGLUtil.PLATFORM_MACOSX) {
+            return MACOSX_NATIVE_LIBRARIES;
+        } else if (platform == org.lwjgl.LWJGLUtil.PLATFORM_LINUX) {
+            return LINUX_NATIVE_LIBRARIES;
+        } else {
+            throw new IllegalStateException("Could not get lwjgl natives for platform \"" + org.lwjgl.LWJGLUtil.getPlatformName() + "\".");
+        }
+    }
+
+    private static String getNativesDirectory() {
+        final int platform = org.lwjgl.LWJGLUtil.getPlatform();
+        if (platform == org.lwjgl.LWJGLUtil.PLATFORM_WINDOWS) {
+            return WINDOWS_NATIVES_DIRECTORY;
+        } else if (platform == org.lwjgl.LWJGLUtil.PLATFORM_MACOSX) {
+            return MACOSX_NATIVES_DIRECTORY;
+        } else if (platform == org.lwjgl.LWJGLUtil.PLATFORM_LINUX) {
+            return LINUX_NATIVES_DIRECTORY;
+        } else {
+            throw new IllegalStateException("Could not get lwjgl natives for platform \"" + org.lwjgl.LWJGLUtil.getPlatformName() + "\".");
+        }
     }
 
     /**
