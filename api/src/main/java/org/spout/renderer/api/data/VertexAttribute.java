51abc358d5593448ea85baab800fe2e54ac1df37
Aleksi Sapon <QcTechs@gmail.com>
2014-05-01 14:22:44 -0400

    Add translation from data format to the equivalent type class

--- api/src/main/java/org/spout/renderer/api/data/VertexAttribute.java
+++ api/src/main/java/org/spout/renderer/api/data/VertexAttribute.java
@@ -227,24 +227,26 @@ public class VertexAttribute implements Cloneable {
      * Represents an attribute data type.
      */
     public static enum DataType {
-        BYTE(0x1400, 1, true), // GL11.GL_BYTE
-        UNSIGNED_BYTE(0x1401, 1, true), // GL11.GL_UNSIGNED_BYTE
-        SHORT(0x1402, 2, true), // GL11.GL_SHORT
-        UNSIGNED_SHORT(0x1403, 2, true), // GL11.GL_UNSIGNED_SHORT
-        INT(0x1404, 4, true), // GL11.GL_INT
-        UNSIGNED_INT(0x1405, 4, true), // GL11.GL_UNSIGNED_INT
-        HALF_FLOAT(0x140B, 2, false), // GL30.GL_HALF_FLOAT
-        FLOAT(0x1406, 4, false), // GL11.GL_FLOAT
-        DOUBLE(0x140A, 8, false); // GL11.GL_DOUBLE
+        BYTE(0x1400, 1, true, true), // GL11.GL_BYTE
+        UNSIGNED_BYTE(0x1401, 1, true, false), // GL11.GL_UNSIGNED_BYTE
+        SHORT(0x1402, 2, true, true), // GL11.GL_SHORT
+        UNSIGNED_SHORT(0x1403, 2, true, false), // GL11.GL_UNSIGNED_SHORT
+        INT(0x1404, 4, true, true), // GL11.GL_INT
+        UNSIGNED_INT(0x1405, 4, true, false), // GL11.GL_UNSIGNED_INT
+        HALF_FLOAT(0x140B, 2, false, true), // GL30.GL_HALF_FLOAT
+        FLOAT(0x1406, 4, false, true), // GL11.GL_FLOAT
+        DOUBLE(0x140A, 8, false, true); // GL11.GL_DOUBLE
         private final int glConstant;
         private final int byteSize;
         private final boolean integer;
+        private final boolean signed;
         private final int multiplyShift;
 
-        private DataType(int glConstant, int byteSize, boolean integer) {
+        private DataType(int glConstant, int byteSize, boolean integer, boolean signed) {
             this.glConstant = glConstant;
             this.byteSize = byteSize;
             this.integer = integer;
+            this.signed = signed;
             multiplyShift = (int) (Math.log(byteSize) / Math.log(2));
         }
 
@@ -273,6 +275,15 @@ public class VertexAttribute implements Cloneable {
          */
         public boolean isInteger() {
             return integer;
+        }
+
+        /**
+         * Returns true if this data type supports signed numbers, false if not.
+         *
+         * @return Whether or not this data type supports signed numbers
+         */
+        public boolean isSigned() {
+            return signed;
         }
 
         /**
--- software/src/main/java/org/spout/renderer/software/DataFormat.java
+++ software/src/main/java/org/spout/renderer/software/DataFormat.java
@@ -26,14 +26,44 @@
  */
 package org.spout.renderer.software;
 
+import com.flowpowered.math.vector.Vector2d;
+import com.flowpowered.math.vector.Vector2f;
+import com.flowpowered.math.vector.Vector2i;
+import com.flowpowered.math.vector.Vector3d;
+import com.flowpowered.math.vector.Vector3f;
+import com.flowpowered.math.vector.Vector3i;
+import com.flowpowered.math.vector.Vector4d;
+import com.flowpowered.math.vector.Vector4f;
+import com.flowpowered.math.vector.Vector4i;
+
 import org.spout.renderer.api.data.VertexAttribute.DataType;
 
 /**
  *
  */
 public class DataFormat {
+    private static final int MAX_LOOKUP_HASH = 1 << 6;
+    private static final Class<?>[] FORMAT_CLASS_LOOKUP = new Class<?>[MAX_LOOKUP_HASH];
     private final DataType type;
     private final int count;
+
+    static {
+        // Integer
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.INT, 1)] = int.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.INT, 2)] = Vector2i.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.INT, 3)] = Vector3i.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.INT, 4)] = Vector4i.class;
+        // Float
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.FLOAT, 1)] = float.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.FLOAT, 2)] = Vector2f.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.FLOAT, 3)] = Vector3f.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.FLOAT, 4)] = Vector4f.class;
+        // Double
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.DOUBLE, 1)] = double.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.DOUBLE, 2)] = Vector2d.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.DOUBLE, 3)] = Vector3d.class;
+        FORMAT_CLASS_LOOKUP[lookupHash(DataType.DOUBLE, 4)] = Vector4d.class;
+    }
 
     public DataFormat(DataType type, int count) {
         this.type = type;
@@ -52,8 +82,21 @@ public class DataFormat {
         return count << type.getMultiplyShift();
     }
 
+    public Class<?> getTypeClass() {
+        return FORMAT_CLASS_LOOKUP[lookupHash(type, count)];
+    }
+
     @Override
     public String toString() {
         return type + "-" + count;
+    }
+
+    private static byte lookupHash(DataType type, int count) {
+        // Format: 00MM ISCC
+        // M: multiply shift, I: integer, S: signed, C: count
+        return (byte) ((type.getMultiplyShift() & 0b11) << 4
+                | ((type.isInteger() ? 1 : 0) & 0b1) << 3
+                | ((type.isSigned() ? 1 : 0) & 0b1) << 2
+                | count & 0b11);
     }
 }
